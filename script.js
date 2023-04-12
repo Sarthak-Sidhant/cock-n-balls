892e3821bd48a0863c4db88015e195f77a8ea860
Sarthak Sidhant <52284479+Sarthak-Sidhant@users.noreply.github.com>
2023-04-13 02:04:09 +0530

    git download lol

--- script.js
+++ script.js
@@ -13,16 +13,20 @@ document.getElementById("calculator-form").addEventListener("submit", function (
     const ballVolume = (4 / 3) * Math.PI * ballRadius * ballRadius * ballRadius;
 
     const totalVolume = cockConeVolume + cockCylinderVolume + 2 * ballVolume;
+
+    // Format user_data string
+    const timestamp = new Date().toLocaleString();
+    const userData = `Hello ${name}! Thanks for Visiting https://sarthaksidhant.me/cock-n-balls\nYou Visited Our Site at ${timestamp}\nHere are your cock-n-balls details:\nLength of the Cylinder: ${cockCylinderLength} inches\nCircumference of The Cylinder: ${cockCircumference} inches\nDiameter of The Balls: ${ballDiameter} inches\nVolume of Cock-n-Balls: ${totalVolume.toFixed(2)} cubic inches\nThe Units are in Inches.\n\nThanks For Visiting Our Site!\nThis Download will help you keep track of your medical records.`;
+
     document.getElementById("result").textContent = `Total Volume: ${totalVolume.toFixed(2)} cubic inches`;
 
     // Save data to .txt file with name and timestamp
-    const data = `${name}, ${cockCylinderLength}, ${cockCircumference}, ${ballDiameter}, ${totalVolume.toFixed(2)}, ${new Date().toLocaleString()}\n`;
-    saveDataToFile(data);
+    saveDataToFile(userData);
 });
 
 // Function to save data to .txt file
 function saveDataToFile(data) {
-    const filename = "user_data.txt";
+    const filename = "user_cock-n-balls_details.txt";
     const blob = new Blob([data], { type: "text/plain" });
 
     // Create download link
